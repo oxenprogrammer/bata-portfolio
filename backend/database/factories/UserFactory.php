23e05ab50ef15171e2b769affd2670c0e204c53f
Kibooli-Felix <kiboolif@gmail.com>
2024-09-18 22:54:40 +0300

    Modify UserFactory to include name field

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'name'=>'Admin',
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail(),
