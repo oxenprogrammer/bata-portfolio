d9a44ab148fb667a53da0b85586016b8c6644776
Kibooli-Felix <kiboolif@gmail.com>
2024-09-25 11:01:09 +0300

    Add routes for Document CRUD operations under admin prefix

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,6 +11,7 @@
  */
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DocumentController;
 
 Route::get(
     '/', function () {
@@ -21,4 +22,11 @@ Route::get(
 // Group admin routes under a common name prefix
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+   // Document routes
+   Route::get('/document/view', [DocumentController::class, 'index'])->name('document.view');
+   Route::get('/document/create', [DocumentController::class, 'create'])->name('document.create');
+   Route::post('/document/store', [DocumentController::class, 'store'])->name('document.store');
+   Route::get('/document/edit/{id}', [DocumentController::class, 'edit'])->name('document.edit');
+   Route::put('/document/update/{id}', [DocumentController::class, 'update'])->name('document.update');
+   Route::delete('/document/destroy/{id}', [DocumentController::class, 'destroy'])->name('document.destroy');
 });
