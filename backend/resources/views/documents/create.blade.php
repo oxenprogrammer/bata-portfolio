4abf70e0aa154607fd498386f6e6892e5b60a148
Kibooli-Felix <kiboolif@gmail.com>
2024-09-25 12:01:20 +0300

    Add document creation and view blade files

---
+++ backend/resources/views/documents/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.dashboard.main')
+<!--title section-->
+@section('title')
+{{$page_title}}
+@endsection
+<!--main content section-->
+@section('content')
+<div class="row">
+    <h2>Document Creation</h2>
+</div>
+@endsection
+<!--scripts section-->
+@section('scripts')
+
+@endsection
