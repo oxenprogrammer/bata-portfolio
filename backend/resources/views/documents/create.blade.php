0bdb637bf89ec4a6d034d33eaf50ce7116e9e63c
Kibooli-Felix <kiboolif@gmail.com>
2024-09-27 00:41:29 +0300

    transform document description field to use tinymyce

--- backend/resources/views/documents/create.blade.php
+++ backend/resources/views/documents/create.blade.php
@@ -64,4 +64,14 @@
 @endsection
 <!--scripts section-->
 @section('scripts')
+    <script>
+        $(document).ready(function () {
+            tinymce.init({
+                selector: '#description', 
+                plugins: 'lists link image',
+                toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image',
+                height: 300 // Adjust height as needed
+            });
+        });
+    </script>
 @endsection
