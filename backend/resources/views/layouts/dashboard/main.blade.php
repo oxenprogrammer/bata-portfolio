47ad9c1a0d7cde416cb5c6bdfb1562fd577ca8c6
Kibooli-Felix <kiboolif@gmail.com>
2024-09-27 00:39:00 +0300

    Include tinymce rich text editor script

--- backend/resources/views/layouts/dashboard/main.blade.php
+++ backend/resources/views/layouts/dashboard/main.blade.php
@@ -128,7 +128,8 @@
 
     <!-- SweetAlert2 JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js"></script>
-
+    <!--tinymce editor-->
+    <script src="{{ asset('tinymce/js/tinymce/tinymce.min.js') }}"></script>
     <script>
         $(function() {
             'use strict'
