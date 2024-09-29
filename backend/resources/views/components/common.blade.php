6f1a4e5ef76989f0069b0c9af465745445c9ca92
Kibooli-Felix <kiboolif@gmail.com>
2024-09-30 00:04:59 +0300

    Modify common file to include id attributes

--- backend/resources/views/components/common.blade.php
+++ backend/resources/views/components/common.blade.php
@@ -1,7 +1,7 @@
 <script>
     $(document).ready(function () {
         tinymce.init({
-            selector: '#description', 
+            selector: '#description,#content,#excerpt', 
             plugins: 'lists link image',
             toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image',
             height: 300 // Adjust height as needed
