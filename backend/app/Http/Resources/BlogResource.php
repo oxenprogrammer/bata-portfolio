f4aef5a40d54f1e0733d31642ee158e381bc5f58
Kibooli-Felix <kiboolif@gmail.com>
2024-10-04 14:25:06 +0300

    Modify blog resource to return modified fields

---
+++ backend/app/Http/Resources/BlogResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BlogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'excerpt' => $this->excerpt,
+            'content' => $this->content,
+            'created_by' => $this->user->first_name . ' ' . $this->user->last_name,
+            'images' => $this->images->map(function ($image) {
+                return [
+                    'id' => $image->id,
+                    'url' => $image->image_path,
+                ];
+            }),
+            'published_at' => $this->created_at->format('M d, Y h:i A'), // Format to human-readable date
+            'updated_at' => $this->updated_at->format('M d, Y h:i A'), // Format to human-readable date
+        ];
+    }
+}
