1f5472d140ee07267bd0b6822eb19f16d31eadd6
Kibooli-Felix <kiboolif@gmail.com>
2024-10-04 11:52:11 +0300

    Modify document api routes to use newly defined methods in the controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentController;
 
+// Publicly accessible API routes
+
+// Route to fetch all documents
+Route::get('/documents', [DocumentController::class, 'getAllDocuments']);
+
+// Route to fetch a specific document by its ID
+Route::get('/documents/{id}', [DocumentController::class, 'getSingleDocument']);
+
+// restricted access api routes
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
