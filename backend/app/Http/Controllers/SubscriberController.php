0f0f902e11a69f8505882cc19275f8a6895384d5
Kibooli-Felix <kiboolif@gmail.com>
2024-10-07 23:36:33 +0300

    Modify index method to return all subscribers and general annotation of class and methods

--- backend/app/Http/Controllers/SubscriberController.php
+++ backend/app/Http/Controllers/SubscriberController.php
@@ -1,17 +1,47 @@
 <?php
 
+/**
+ * This File contains Subscriber Controller and its methods
+ * to handle subscriptions
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
+
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
+/**
+ * This File Class handles subscribers
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
 class SubscriberController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Return all subscribers
+     *
+     * @return void
      */
     public function index()
     {
         //
+        $page_title = "Admin Panel Subscribers";
+        $subscribers = Subscriber::all();
+        return view('subscribers.index', compact('page_title', 'subscribers'));
     }
 
     /**
