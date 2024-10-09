917beb6806774a044a955ad3510756ed63753df6
Kibooli-Felix <kiboolif@gmail.com>
2024-10-09 15:26:16 +0300

    Add susbscriber post route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\SubscriberController;
 
 // Publicly accessible API routes
 
@@ -17,6 +18,9 @@ Route::get('/blogs', [BlogController::class, 'getAllBlogs']);
 
 // Route to fetch a specific document by its ID
 Route::get('/blogs/{id}',[BlogController::class, 'getSingleBlog']);
+
+//Route to create subscriber
+Route::post('/subscriber',[SubscriberController::class,'store']);
 
 
 // restricted access api routes
