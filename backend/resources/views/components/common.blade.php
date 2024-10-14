644d8b195b56af5d9d8d772f04c7bf17bbdeae02
Kibooli-Felix <kiboolif@gmail.com>
2024-10-15 00:25:11 +0300

    update common blade file

--- backend/resources/views/components/common.blade.php
+++ backend/resources/views/components/common.blade.php
@@ -1,7 +1,7 @@
 <script>
     $(document).ready(function () {
         tinymce.init({
-            selector: '#description,#content,#excerpt', 
+            selector: '#description,#content,#excerpt,#response', 
             plugins: 'lists link image',
             toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image',
             height: 300 // Adjust height as needed
