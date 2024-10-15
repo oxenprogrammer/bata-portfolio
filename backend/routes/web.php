535087b9ca6fe7d0afdbc7e32dd32eb394189575
Kibooli-Felix <kiboolif@gmail.com>
2024-10-15 23:10:42 +0300

    Modify routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -49,8 +49,8 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/contacts/view',[ContactUsController::class,'index'])->name('contact.view');
     Route::get('/contact/details/{id}', [ContactUsController::class, 'show'])->name('contact.detail');
     Route::delete('/contact/destroy/{id}', [ContactUsController::class, 'destroy'])->name('contact.destroy');
-    Route::get('/contact/show/{id}', [ContactUsController::class, 'show'])->name('contact.show');
-
+    Route::post('/contact/reply/{id}', [ContactUsController::class, 'reply'])->name('contact.reply');
+    Route::post('/contact/replied/{id}', [ContactUsController::class, 'markReplied'])->name('contact.mark-replied');
 });
 
 
