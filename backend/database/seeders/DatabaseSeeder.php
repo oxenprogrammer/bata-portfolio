ec2e4f9bdb64fdf7e2fd0b8ffb921d8bc751477b
Kibooli-Felix <kiboolif@gmail.com>
2024-10-24 23:59:52 +0300

    Modify DatabaseSeeder to seed admin user

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,13 +17,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         // Create an admin user
-        User::factory()->create([
-            'name'=>'Admin',
+        // dd(\Faker\Factory::create('en_us'));
+        // User::factory()->create([
+        //     'name'=>'Admin',
+        //     'first_name' => 'Kibooli',
+        //     'last_name'=>'Felix',
+        //     'email' => 'kiboolif@gmail.com',
+        //     'password' => bcrypt('christosestinkalon'),
+        //     'is_admin' => true,
+        // ]);
+         // Directly insert an admin user into the users table
+         DB::table('users')->insert([
+            'name' => 'Admin',
             'first_name' => 'Kibooli',
-            'last_name'=>'Felix',
+            'last_name' => 'Felix',
             'email' => 'kiboolif@gmail.com',
-            'password' => bcrypt('christosestinkalon'),
+            'password' => Hash::make('christosestinkalon'),
             'is_admin' => true,
+            'email_verified_at' => now(),
+            'remember_token' => null,
         ]);
     }
 }
