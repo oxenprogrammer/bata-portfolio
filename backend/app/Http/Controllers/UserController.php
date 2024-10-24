cb10e3324c1fcabcc90447ceda66952f411e8bd3
Kibooli-Felix <kiboolif@gmail.com>
2024-10-25 01:02:11 +0300

    Create UserController with resource methods, add index method to return users

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This File contains Users Controller and its methods
+ * 
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+/**
+ * This is  Users Controller class 
+ * 
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
+
+class UserController extends Controller
+{
+    /**
+     * Return all users
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+        $page_title = 'Admin Panel View Users';
+        $users = User::all();
+        return view('users.index', compact('users', 'page_title'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
