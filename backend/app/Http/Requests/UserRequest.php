093b94d0dcf8b11c26eb6d8d0c0b77f3209e15e7
Kibooli-Felix <kiboolif@gmail.com>
2024-10-25 02:35:19 +0300

    Refactor Request class

--- backend/app/Http/Requests/UserRequest.php
+++ backend/app/Http/Requests/UserRequest.php
@@ -11,7 +11,7 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -27,7 +27,7 @@ class UserRequest extends FormRequest
             'email' => 'required|email|unique:users,email',
             'biography' => 'nullable|string',
             'skills' => 'nullable|string',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:8',
             'is_admin' => 'nullable|boolean',
         ];
     }
