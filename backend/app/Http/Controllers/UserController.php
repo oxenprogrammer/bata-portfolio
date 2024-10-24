c4f7bc5007150294b8d1a6489892d2ec8f1ca4db
Kibooli-Felix <kiboolif@gmail.com>
2024-10-25 02:34:10 +0300

    Refactor index method to return skills in json form

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -15,8 +15,13 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Log;
+
+use function PHPSTORM_META\map;
 
 /**
  * This is  Users Controller class 
@@ -42,7 +47,10 @@ class UserController extends Controller
     {
         //
         $page_title = 'Admin Panel View Users';
-        $users = User::all();
+        $users = User::all()->map(function ($user) {
+            $user->skills = $user->skills ? json_decode($user->skills) : [];
+            return $user;
+        });
         return view('users.index', compact('users', 'page_title'));
     }
 
@@ -59,9 +67,30 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         //
+        try {
+            $validatedData = $request->validated();
+
+            $user = User::create([
+                'name' => $validatedData['name'] ?? null, 
+                'first_name' => $validatedData['first_name'],
+                'last_name' => $validatedData['last_name'],
+                'email' => $validatedData['email'],
+                'biography' => $validatedData['biography'],
+                'skills' => $validatedData['skills'],
+                'password' => bcrypt($validatedData['password']),
+                'is_admin' => $validatedData['is_admin'] ?? false,
+            ]);
+
+            return redirect()->route('admin.user.view')->with('success', 'User created successfully.');
+
+        } catch (Exception $e) {
+            Log::error('Error while creating user: ' . $e->getMessage());
+
+            return redirect()->back()->withErrors(['error' => 'An error occurred while creating the user.']);
+        }
     }
 
     /**
