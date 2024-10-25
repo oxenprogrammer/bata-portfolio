ebec9188eca784a00f2ecf32fe424174af9f65e7
Kibooli-Felix <kiboolif@gmail.com>
2024-10-25 03:45:01 +0300

    Modify destroy method to delete user

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -135,5 +135,8 @@ class UserController extends Controller
     public function destroy(string $id)
     {
         //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('admin.user.view')->with('success', 'User deleted successfully!');
     }
 }
