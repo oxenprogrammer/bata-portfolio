9d2f990246ebf245b8a80d1e0c7e7ed56cf47bea
Kibooli-Felix <kiboolif@gmail.com>
2024-10-30 21:18:39 +0300

    Add file and class annotation

--- backend/app/Http/Controllers/NewsletterController.php
+++ backend/app/Http/Controllers/NewsletterController.php
@@ -1,10 +1,35 @@
 <?php
 
+/**
+ * This File contains Newsletter Controller and its methods
+ * 
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * This is  Newsletter Controller class 
+ * 
+ *
+ * PHP version 8
+ *
+ * @category  Controllers
+ * @package    App\Http\Controllers
+ * @author     Kibooli Felix <kiboolif@gmail.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @link       https://github.com/KIBOOLI-FELIX/mribrahimsite.git
+ */
 class NewsletterController extends Controller
 {
-    //
+
 }
