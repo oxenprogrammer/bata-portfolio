3a1e3a8ce77fdd0ce6845a894e9bcaab77b3c8c4
Kibooli-Felix <kiboolif@gmail.com>
2024-11-03 19:32:29 +0300

    Refactor index blade file

--- backend/resources/views/newsletters/index.blade.php
+++ backend/resources/views/newsletters/index.blade.php
@@ -72,6 +72,7 @@
                                 @if(!$newsletter->is_sent)
                                     <form action="{{ route('admin.newsletter.destroy', $newsletter->id) }}" method="POST" class="d-inline-block">
                                         @csrf
+                                        @method('DELETE')
                                         <button type="submit" class="btn btn-sm btn-danger">
                                             <i class="fas fa-trash"></i> Delete
                                         </button>
