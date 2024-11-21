844c7b0a42e2419f34f84491bead4e85e45e1639
Kibooli-Felix <kiboolif@gmail.com>
2024-11-21 10:59:07 +0300

    Add unsubscribe view

---
+++ backend/resources/views/subscribers/unsubscribe.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Subscription Status</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body text-center">
+                        @if(!$substatus)
+                            <h3 class="card-title text-success">Subscription Status</h3>
+                            <p class="card-text">You have successfully unsubscribed</p>
+                        @else
+                            <h3 class="card-title text-warning"></h3>
+                            <p class="card-text">It looks like you've have already unsubscribed.</p>
+                        @endif
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
